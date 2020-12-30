3cb5c17dda8c12f72896bda26863dc0380a63f71
Jakub Bebenek <jakub.bebenek@gmail.com>
2020-12-30 00:53:54 +0000

    Initial Commit for Better Rendering

--- app/screens/RenderScreen.js
+++ app/screens/RenderScreen.js
@@ -9,6 +9,7 @@ import { GLView } from 'expo-gl';
 // https://ssd-api.jpl.nasa.gov/doc/sbdb.html
 
 global.THREE = global.THREE || THREE;
+    
 
 export default class RenderScreen extends Component {
   constructor(props) {
@@ -18,7 +19,26 @@ export default class RenderScreen extends Component {
       orbitData: null,
       error: null,
       windowWidth: Dimensions.get('window').width,
-      windowHeight: Dimensions.get('window').height
+      windowHeight: Dimensions.get('window').height,
+      star: new THREE.Mesh(
+        new THREE.SphereBufferGeometry(20, 32, 32),
+        new THREE.MeshBasicMaterial({map: ExpoTHREE.loadAsync(require("../../assets/sun-texture.jpg"))}),
+      ),
+      planetGeom: new THREE.Mesh(
+        new THREE.SphereBufferGeometry(5, 32, 32),
+        new THREE.MeshBasicMaterial({map: ExpoTHREE.loadAsync(require("../../assets/earth-texture.jpg"))})
+      ),
+      planet: new THREE.Object3D(),
+      orbit: new THREE.Line(
+        new THREE.CircleGeometry(100, 90),
+        new THREE.MeshBasicMaterial({
+          color: 0xffffff,
+          transparent: true,
+          opacity: .05,
+          side: THREE.BackSide
+        })
+      ),
+      light1: new THREE.PointLight(0x090909, 2, 0, 0)
     };
   }
 
@@ -33,90 +53,33 @@ export default class RenderScreen extends Component {
     //Objects
 
     // SUN
-    let starColor = (function() {
-      let colors = [0xFFFF00, 0x559999, 0xFF6339, 0xFFFFFF];
-      return colors[Math.floor(Math.random() * colors.length)];
-    })(),
-
-    star = new THREE.Mesh(
-      new THREE.SphereBufferGeometry(20, 32, 32),
-      new THREE.MeshBasicMaterial({map: await ExpoTHREE.loadAsync(require("../../assets/sun-texture.jpg"))})
-    ),
-    glows = [];
-
-    star.castShadow = false;
+    this.state.star.castShadow = false;
 
     // EARTH
-    
-    let planetGeom = new THREE.Mesh(
-      new THREE.SphereBufferGeometry(5, 32, 32),
-      new THREE.MeshBasicMaterial({map: await ExpoTHREE.loadAsync(require("../../assets/earth-texture.jpg"))})
-    ),
-    planet = new THREE.Object3D();
 
-    planet.add(planetGeom);
+    this.state.planet.add(this.state.planetGeom);
+    this.state.planet.orbitRadius = 50 + 50 + 0;
+    this.state.planet.rotSpeed = 0.005 + 0.01;
+    this.state.planet.rot = Math.random();
+    this.state.planet.orbitSpeed = (0.02 - 0 * 0.0048) * 0.25;
+    this.state.planet.orbit = Math.random() * Math.PI * 2;
+    this.state.planet.position.set(this.state.planet.orbitRadius, 0, 0);
 
-    // if (type > 1 && Math.random() > 0.5) {
-    //   var atmoGeom = new THREE.Mesh(
-    //     new THREE.SphereBufferGeometry(5, 32, 32),
-    //     new THREE.MeshLambertMaterial({
-    //       color: planetColors[3],
-    //       shading: THREE.FlatShading,
-    //       transparent: true,
-    //       opacity: 0.5
-    //     })
-    //   );
-
-    //   atmoGeom.castShadow = false;
-    //   planet.add(atmoGeom);
-    // }
-
-    planet.orbitRadius = 50 + 50 + 0;
-    planet.rotSpeed = 0.005 + 0.01;
-    planet.rot = Math.random();
-    planet.orbitSpeed = (0.02 - 0 * 0.0048) * 0.25;
-    planet.orbit = Math.random() * Math.PI * 2;
-    planet.position.set(planet.orbitRadius, 0, 0);
-
-    var orbit = new THREE.Line(
-      new THREE.CircleGeometry(planet.orbitRadius, 90),
-      new THREE.MeshBasicMaterial({
-        color: 0xffffff,
-        transparent: true,
-        opacity: .05,
-        side: THREE.BackSide
-      })
-    );
-    orbit.geometry.vertices.shift();
-    orbit.rotation.x = THREE.Math.degToRad(90);
-    scene.add(orbit);
-
-    let radii = planet.orbitRadius + 0;
-    scene.add(planet);
+    this.state.orbit.geometry.vertices.shift();
+    this.state.orbit.rotation.x = THREE.Math.degToRad(90);
 
     //Lights
-    let light1 = new THREE.PointLight(starColor, 2, 0, 0);
+    this.state.light1.position.set(0, 0, 0);
 
-    light1.position.set(0, 0, 0);
-    scene.add(light1);
-
-    let light2 = new THREE.AmbientLight(0x090909);
-    scene.add(light2);
-
-    // for (let s in bgStars) {
-    //   let q = bgStars[s],
-    //     oX = q.x * this.state.windowWidth,
-    //     oY = q.y * this.state.windowHeight,
-    //     size = Math.random() < .9998 ? Math.random() : Math.random() * 3;
-    // }
-
-
-    
-    scene.add(star);
+    scene.add(this.state.orbit);
+    scene.add(this.state.planet);
+    scene.add(this.state.light1);
+    scene.add(this.state.star);
     camera.position.set(0, 20, 150);
+
     const render = () => {
       requestAnimationFrame(render);
-      star.rotation.y += 0.0025;
+      this.state.star.rotation.y += 0.0025;
       renderer.render(scene, camera);
       gl.endFrameEXP();
     };
@@ -146,6 +109,8 @@ export default class RenderScreen extends Component {
                 console.log(this.state.error);
             }
     }
+
+
   }
 
 
