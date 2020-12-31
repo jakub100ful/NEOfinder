709582801716450f0ba3276a09ce68c3937ada6e
Jakub Bebenek <jakub.bebenek@gmail.com>
2020-12-31 22:36:26 +0000

    Added Button Assets

--- app/screens/IntroScreen.js
+++ app/screens/IntroScreen.js
@@ -1,5 +1,33 @@
 import React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, Image, Dimensions } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import MainTabNavigator from './MainTabNavigator';
+
+// const starTile = require('../../assets/bg-star-tile.png');
+
+// let RepeatImage = React.createClass({
+//     render: function(){
+//     let images = [],  
+//     imgWidth = 7,
+//     winWidth =Dimensions.get('window').width;
+
+//     for(let i=0;i<Math.ceil(winWidth / imgWidth);i++){
+//         images.push((
+//            <Image source={starTile}/>
+//         ))
+//     }
+
+//     return (
+//         <View style={{flex:1,flexDirection:'row'}}>
+//         {
+//          images.map(function(img,i){
+//          return img;
+//          })
+//         }
+//         </View>
+//     )
+//   }
+// });
 
 function IntroScreen(props) {
     return (
@@ -8,7 +36,9 @@ function IntroScreen(props) {
                 <Image style={styles.stretch} source={require('../../assets/neo-finder-logo.png')}/>
             </View>
             <View style={styles.buttonView}>
-
+                <TouchableOpacity onPress={()=>{return <MainTabNavigator/>}}>
+                    <Image style={styles.startButton} source={require('../../assets/start-button.png')}/>
+                </TouchableOpacity>
             </View>
         </View>
     );
@@ -17,17 +47,19 @@ function IntroScreen(props) {
 const styles = new StyleSheet.create({
     mainContainer: {
         flex: 1,
-        flexDirection: 'row',
         backgroundColor: "black"
     },
     logoView: {
-        flex: 1
+        flex: 2,
+        justifyContent: "center"
     },
     buttonView: {
+        flex: 1,
+        alignItems: 'center'
     },
     stretch: {
         width: "100%",
-        height: "60%",
+        height: "80%",
         resizeMode: "stretch"
     }
 })
