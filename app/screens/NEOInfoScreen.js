b774858290c221256acd0fb07656d1b67bffab23
jakub100ful <56305120+jakub100ful@users.noreply.github.com>
2021-01-26 00:58:30 +0000

    Cleaning up NEO Info Screen

--- app/screens/NEOInfoScreen.js
+++ app/screens/NEOInfoScreen.js
@@ -5,18 +5,11 @@ import React from "react";
 import CustomButton from "../components/CustomButton";
 import NEORenderPreview from "../components/NEORenderPreview";
 import fetchNEOOrbitData from "../functions/fetchNEOOrbitData";
-import getSizeComparison from "../functions/getSizeComparison";
 
 
 export default function NEOInfoScreen (props){
   const [NEO, setNEO] = useState(null);
   const [tempNEO, setTempNEO] = useState(null);
-  const [sizeComparison, setSizeComparison] = useState(null);
-
-  const getComparison = async (measurement, measurementUnit) => {
-    const comparison = await getSizeComparison(measurement, measurementUnit);
-    setSizeComparison(comparison);
-  }
 
   useEffect(()=>{
     const fetchData = async () => {
@@ -32,16 +25,22 @@ export default function NEOInfoScreen (props){
     }
   }, [])
 
+  /**
+   * Fetches data for given NEO object and ensures it contains approach data for the 
+   * soonest or most recent period.
+   */
   useEffect(()=>{
     if (tempNEO != null){
       const currentDate = new Date();
       let stateCopy = tempNEO;
       let closestApproachDate;
-      let tempDate;
       
+      // Runs through individual approach dates
       for (let approachDate of stateCopy.close_approach_data){
         let formattedDate = Date.parse(approachDate.close_approach_date);
 
+        // If date is larger than current date, use it and break the loop,
+        // if not save it and continue the loop.
         if (formattedDate > currentDate){
           closestApproachDate = approachDate;
           break;
@@ -50,19 +49,14 @@ export default function NEOInfoScreen (props){
         }
       }
   
+      // Modify the NEO object so that it only contains the relevant approach date
       stateCopy.close_approach_data = [closestApproachDate];
-
       setNEO(stateCopy)
     }
   },[tempNEO])
 
-  useEffect(() => {
-    if (sizeComparison == null & NEO){
-      getComparison(NEO.estimated_diameter.meters.estimated_diameter_min, "meters")
-    }
-  }, [])
-
-  if (NEO == null & sizeComparison == null){
+  // Conditional rendering
+  if (NEO == null){
     return(
       <View>
         <Text>Loading</Text>
@@ -71,45 +65,57 @@ export default function NEOInfoScreen (props){
   }else{
     return(
 
+      // Background
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "black" }}>
         <Image style={styles.background} source={require('../../assets/star-bg.png')}/>
         
-        
+        {/* Body */}
         <View style={styles.body}>
           <View style={styles.NEOinfoView}>
             
+            {/* Intro view - displays name */}
             <View style={styles.introView}>
-
               <View style={styles.introInfoView}>
                 <Text style={styles.header}>{NEO.name}</Text>
               </View>
-
             </View>
-              <View style={styles.NEOPreview}>
-                <NEORenderPreview NEO={NEO}/>
-              </View>
+
+            {/* NEO Preview - displays preview of the NEO */}
+            <View style={styles.NEOPreview}>
+              <NEORenderPreview NEO={NEO}/>
+            </View>
+
+            {/* NEO Info - displays information about the close approach of the NEO */}
               <View style={styles.sectionView}>
                 <Text style={styles.sectionHeader}>Close Approach Data</Text>
+
+                {/* Section Body */}
                 <View style={styles.sectionBody}>
+
+                  {/* Text and subtext components for displaying data */}
                   <Text style={styles.mainText}>Hazardous:</Text>
                   <Text style={styles.subText}>{NEO["is_potentially_hazardous_asteroid"] ? "Yes" : "No"}</Text>
+                  
                   <Text style={styles.mainText}>Orbiting Body:</Text>
                   <Text style={styles.subText}>{NEO.close_approach_data[0].orbiting_body}</Text>
+                  
                   <Text style={styles.mainText}>Close Approach Date:</Text>
                   <Text style={styles.subText}>{NEO.close_approach_data[0].close_approach_date}</Text>
+                  
                   <Text style={styles.mainText}>Miss Distance:</Text>
                   <Text style={styles.subText}>{Math.round(NEO.close_approach_data[0].miss_distance.kilometers*100)/100} km</Text>
+                  
                   <Text style={styles.mainText}>Relative Velocity:</Text>
                   <Text style={styles.subText}>{Math.round(NEO.close_approach_data[0].relative_velocity.kilometers_per_second*100)/100} km/s</Text>
+                  
                   <Text style={styles.mainText}>Estimated Diameter:</Text>
-                  <Text style={styles.subText}>{Math.round(NEO.estimated_diameter.meters.estimated_diameter_min*100)/100} m</Text>
-                  <Text style={styles.subText}>{sizeComparison}</Text>
+                  <Text style={styles.subText}>{Math.round(NEO.estimated_diameter.meters.estimated_diameter_min*100)/100} m</Text>                  
               </View>
             </View>
           </View>
-
         </View>
-        {/* <OrbitView width={windowWidth} height={windowHeight}/> */}
+
+        {/* Back Button */}
         <View style= {{flex: 1, marginTop: "7%", flexDirection: "row"}}>
           <CustomButton title="Back" callback={()=>{props.navigation.goBack()}}/>
         </View>
@@ -120,7 +126,7 @@ export default function NEOInfoScreen (props){
 
 const styles = StyleSheet.create({
   body: {
-    flex: 11,
+    flex: 12,
     width: "90%",
     alignItems: "center",
     justifyContent: "center"
@@ -136,7 +142,6 @@ const styles = StyleSheet.create({
   },
   sectionView: {
     backgroundColor: 'rgba(255, 255, 255, 0.15)',
-    flex: 2,
     marginBottom: 5
   },
   header: {
@@ -155,7 +160,7 @@ const styles = StyleSheet.create({
     padding: 5
 },
   mainText: {
-    fontSize: 27,
+    fontSize: 30,
     fontFamily: "8-bit-Arcade-In",
     color: "white"
   },
@@ -181,7 +186,7 @@ const styles = StyleSheet.create({
   },
   sectionBody: {
     width: "90%",
-    padding: 10
+    paddingLeft: 10
   }
   
 })
